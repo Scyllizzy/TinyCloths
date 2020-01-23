17b79df57758025ee55d5c644e5a44444d3b590f
Scyllizzy <babytink2002@hotmail.com>
2020-01-23 14:53:51 -0800

    Created a clothing Add method

--- TinyClothes/Data/ClothingDB.cs
+++ TinyClothes/Data/ClothingDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TinyClothes.Models;
 
 namespace TinyClothes.Data
 {
@@ -10,6 +11,23 @@ namespace TinyClothes.Data
     /// </summary>
     public static class ClothingDB
     {
+        public static List<Clothing> GetAllClothing()
+        {
+            throw new NotImplementedException();
+        }
 
+        /// <summary>
+        /// Adds a clothing object to the database
+        /// Return the object with the ID populated
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static Clothing Add(Clothing c, StoreContext context)
+        {
+            context.Add(c); // prepares INSERT query
+            context.SaveChanges(); // execute INSERT query
+
+            return c;
+        }
     }
 }
