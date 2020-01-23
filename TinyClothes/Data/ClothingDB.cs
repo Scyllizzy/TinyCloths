c41dce2f9304c041fce463666182fcae57d30003
Scyllizzy <babytink2002@hotmail.com>
2020-01-23 10:39:53 -0800

    Started Clothing Add page

---
+++ TinyClothes/Data/ClothingDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Data
+{
+    /// <summary>
+    /// Contains DB helper methods for <see cref="Models.Clothing"/>
+    /// </summary>
+    public static class ClothingDB
+    {
+
+    }
+}
