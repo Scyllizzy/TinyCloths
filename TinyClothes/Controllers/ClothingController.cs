f7c29a444a2e0d7bc406d1875079ef661a55c7f2
Scyllizzy <babytink2002@hotmail.com>
2020-01-28 09:27:45 -0800

    Added clothes controller

--- TinyClothes/Controllers/ClothingController.cs
+++ TinyClothes/Controllers/ClothingController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
 
 namespace TinyClothes.Controllers
 {
     public class ClothingController : Controller
     {
+        private readonly StoreContext _context;
+
+        public ClothingController(StoreContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult InventoryList()
         {
             return View();
