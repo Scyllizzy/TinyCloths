1bb77970bf377e3c5ea142f407e2ad454e3bb21b
Scyllizzy <babytink2002@hotmail.com>
2020-02-04 20:42:32 -0800

    Added DB to new computer

---
+++ TinyClothes/Migrations/20200205044111_AddDBToNewComputer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinyClothes.Migrations
+{
+    public partial class AddDBToNewComputer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
