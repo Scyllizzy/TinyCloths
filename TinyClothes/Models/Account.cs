c827cdc9a8cda4471fcaeedd4acde4998eaf0b85
Alexia Haley <haley7734@student.cptc.edu>
2020-02-11 10:49:41 -0800

    Created accounts and added to the DBContext

---
+++ TinyClothes/Models/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// A single user account
+    /// </summary>
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+
+        /// <summary>
+        /// First and last name
+        /// </summary>
+        [Required]
+        [StringLength(60)]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DataType(DataType.Password)] // <input tpye="password" />
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Adddress { get; set; }
+    }
+}
