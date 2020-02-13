686a9795193f17b8a90e7584eda3516781a807c3
Alexia Haley <haley7734@student.cptc.edu>
2020-02-13 10:49:19 -0800

    Accounts are added to the db when registered

---
+++ TinyClothes/Data/AccountDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes
+{
+    public static class AccountDB
+    {
+        public static async Task<bool> IsUsernameTaken(string username, StoreContext context)
+        {
+            return await (from a in context.Account where username == a.Username select a).AnyAsync();
+        }
+
+        public static async Task<Account> Register(Account acc, StoreContext context)
+        {
+            await context.Account.AddAsync(acc);
+            await context.SaveChangesAsync();
+
+            return acc;
+        }
+    }
+}
