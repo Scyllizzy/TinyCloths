7c5cdec65fb9ab78b1a4cb09ae9994f35ea5fda4
Scyllizzy <babytink2002@hotmail.com>
2020-02-27 10:22:07 -0800

    Displaying number of cart items next to the cart icon

--- TinyClothes/Models/CartHelper.cs
+++ TinyClothes/Models/CartHelper.cs
@@ -12,6 +12,8 @@ namespace TinyClothes.Models
     /// </summary>
     public static class CartHelper
     {
+        private const string CartCookie = "CartCookie";
+
         public static void Add(Clothing c, IHttpContextAccessor http)
         {
             string data = JsonConvert.SerializeObject(c);
@@ -23,12 +25,19 @@ namespace TinyClothes.Models
                 Secure = true
             };
 
-            http.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
         }
 
         public static int GetItemCount(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            string data = http.HttpContext.Request.Cookies[CartCookie];
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return 0;
+            }
+
+            return 1;
         }
 
         public static List<Clothing> GetAllClothes(IHttpContextAccessor http)
