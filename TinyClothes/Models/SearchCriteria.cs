b1632a9452baa7da88a208ebf469de086db11242
Scyllizzy <babytink2002@hotmail.com>
2020-03-03 10:21:45 -0800

    Created search criteria model

---
+++ TinyClothes/Models/SearchCriteria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    public class SearchCriteria
+    {
+        public SearchCriteria()
+        {
+            Results = new List<Clothing>();
+        }
+
+        public string Size { get; set; }
+
+        /// <summary>
+        /// Type of clothing (shirt, pants, hat, etc...)
+        /// </summary>
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public double? MinPrice { get; set; }
+
+        public double? MaxPrice { get; set; }
+
+        public List<Clothing> Results { get; set; }
+    }
+}
