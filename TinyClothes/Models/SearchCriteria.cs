fa6ce2ef76fd7c98a99adb006c42c4126966e2e8
Scyllizzy <babytink2002@hotmail.com>
2020-03-05 10:12:02 -0800

    Added validation to search page

--- TinyClothes/Models/SearchCriteria.cs
+++ TinyClothes/Models/SearchCriteria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace TinyClothes.Models
 {
     public class SearchCriteria
     {
+        private const string MinPriceName = "Min Price";
+        private const string MaxPriceName = "Max Price";
+
         public SearchCriteria()
         {
             Results = new List<Clothing>();
@@ -19,10 +23,15 @@ namespace TinyClothes.Models
         /// </summary>
         public string Type { get; set; }
 
+        [StringLength(150)]
         public string Title { get; set; }
 
+        [Display(Name = MinPriceName)]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Min value must be positive")]
         public double? MinPrice { get; set; }
 
+        [Display(Name = MaxPriceName)]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Max value must be positive")]
         public double? MaxPrice { get; set; }
 
         public List<Clothing> Results { get; set; }
