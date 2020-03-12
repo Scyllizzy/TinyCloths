7f25e9dbe191b4ca08379c9db06a4bdc60231686
Scyllizzy <babytink2002@hotmail.com>
2020-03-12 10:57:38 -0700

    Started implementing add to cart w javascript

--- TinyClothes/Controllers/CartController.cs
+++ TinyClothes/Controllers/CartController.cs
@@ -40,6 +40,11 @@ namespace TinyClothes.Controllers
             return Redirect(prevUrl); 
         }
 
+        public async void AddJS(int ID)
+        {
+
+        }
+
         // Summary / checkout page
         public IActionResult Checkout()
         {
